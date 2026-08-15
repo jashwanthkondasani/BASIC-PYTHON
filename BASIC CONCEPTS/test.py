6d8fa8d4f090c2ed4cea523b5d6573e9328ae090
jashwanthkondasani <jashwanthkumarreddy53@gmail.com>
2026-08-15 21:55:29 +0530

    ind

--- BASIC CONCEPTS/test.py
+++ BASIC CONCEPTS/test.py
@@ -1,8 +1,14 @@
-def largest(num):
-  largest=num[0]
+# def largest(num):
+#   largest=num[0]
+#   for i in range(1,len(num)):
+#     if num[i]>largest:
+#       largest=num[i]
+#   return largest
+# nums=[1,3,4,5,6]
+# print(largest(nums))
+def smallest(num):
+  smallest=num[0]
   for i in range(1,len(num)):
-    if num[i]>largest:
-      largest=num[i]
-  return largest
-nums=[1,3,4,5,6]
-print(largest(nums))
+    if num[i]<smallest:
+      smallest=num[i]
+  return smallest
